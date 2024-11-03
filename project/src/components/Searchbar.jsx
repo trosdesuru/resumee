4a1231ce67a55f77e57b8e41f97aed740a0d4fc6
trosdesuru <eduardhernandezventos@gmail.com>
2024-11-03 18:59:22 +0100

    fixed problems importation SearchBar.jsx, #cv

--- project/src/components/Searchbar.jsx
+++ project/src/components/Searchbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 
 const SearchBar = ({ onSearch }) => {
   console.debug('search bar -> call')
-
   const [query, setQuery] = useState()
 
   const handleSearch = (event, query) => {
--- project/src/components/VsCodeBar.jsx
+++ project/src/components/VsCodeBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar.jsx'
 
 import { BiDockLeft, BiDockBottom, BiDockRight, BiColumns } from 'react-icons/bi'
 
