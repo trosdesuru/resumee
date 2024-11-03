38554dbfba4affef9b64e2b73612502ba5bc3e8d
trosdesuru <eduardhernandezventos@gmail.com>
2024-11-03 18:56:32 +0100

    problem fixed on SearchBar, #cv

--- project/src/components/Searchbar.jsx
+++ project/src/components/Searchbar.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
 const SearchBar = ({ onSearch }) => {
   console.debug('search bar -> call')
 
-  const [query, setQuery] = useState('')
-  
+  const [query, setQuery] = useState()
+
   const handleSearch = (event, query) => {
     event.preventDefault()
 
